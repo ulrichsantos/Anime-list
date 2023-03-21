bb4d6d150380dc122e85b0719a9ca5bdc1c6b079
Ulrich <ulrich_isantos@hotmail.com>
2023-03-21 18:56:26 -0300

    criando formulario

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,11 +1,78 @@
 import styles from './Modal.module.css'
+import sgMail from '@sendgrid/mail'
+import { useState } from 'react'
+
 
 interface ModalProps {
   isOpen: boolean
   onClose: () => void
 }
 
+interface EmailData {
+  to: string
+  from: string
+  subject: string
+  text: string
+}
+
+function sendEmail(data: EmailData) {
+  const apiKey = process.env.SENDGRID_API_KEY;
+
+  if (!apiKey) {
+    throw new Error('API key not found');
+  }
+
+  sgMail.setApiKey(apiKey);
+
+  sgMail.send(data)
+    .then(() => {
+      alert('Email enviado com sucesso');
+    })
+    .catch((error) => {
+      alert('Error');
+    });
+}
+
 export function Modal( {isOpen, onClose}: ModalProps ) {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [anime, setAnime] = useState('')
+  const [releaseDate, setReleaseDate] = useState('')
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value)
+  }
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value)
+  } 
+
+  const handleAnimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAnime(event.target.value)
+  }
+
+  const handleReleaseDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setReleaseDate(event.target.value)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const data: EmailData = {
+      to: email,
+      from: process.env.FROM_EMAIL || 'default@email.com',
+      subject: 'Novo anime inscrito!',
+      text: `
+        Nome: ${name}
+        Email: ${email}
+        Anime: ${anime}
+        Lançamento do próximo episódio: ${releaseDate}
+      `
+    }
+    sendEmail(data);
+    
+  };
+
   if (!isOpen) {
     return null
   }
@@ -14,11 +81,11 @@ export function Modal( {isOpen, onClose}: ModalProps ) {
     <div className={styles.modal}>
       <div className={styles.modalContainer}>
         <h1>Se inscreva e receba notificações do lançamento desse anime!</h1>
-        <form>
-          Nome: <input type="text" />
-          Email: <input type="email" />
-          Anime: <input type="text" />
-          Lançamento do próximo episódio: <input type="date" />
+        <form onSubmit={handleSubmit}>
+          Nome: <input type="text" value={name} onChange={handleNameChange} />
+          Email: <input type="email" value={email} onChange={handleEmailChange}/>
+          Anime: <input type="text" value={anime} onChange={handleAnimeChange} />
+          Lançamento do próximo episódio: <input type="date" value={releaseDate} onChange={handleReleaseDateChange}/>
           <div className={styles.buttonModal}>
             <button type="submit">Adicionar</button>
             <button onClick={onClose}>Cancelar</button>
